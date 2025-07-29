f5d966df44b46119efac6e6f47f0b59715d950d5
Rajagopika <rajagopika4@gmail.com>
2025-07-29 22:10:06 +0530

    Pushing full backend to GitHub

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ app.use(cors({
   credentials: true
 }));
 // Adjust for production
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 app.use('/images', express.static('images')); // Serving static images
 
 
